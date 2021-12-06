37fd484936c70c3a882ef7b8339aadf508451641
davidangarita1 <davidangarita.sistemas@gmail.com>
2021-12-05 20:51:16 -0500

    Taller de Jest Mock Terminado

---
+++ src/__test__/mock_jest_mock.test.js
@@ -0,0 +1,13 @@
+import * as app from "../App";
+import * as math from "../components/math";
+
+// Set all module functions to jest.fn
+jest.mock("../components/math.js");
+test("calls math.add", () => {
+  app.doAdd(1, 2);
+  expect(math.add).toHaveBeenCalledWith(1, 2);
+});
+test("calls math.subtract", () => {
+  app.doSubtract(1, 2);
+  expect(math.subtract).toHaveBeenCalledWith(1, 2);
+});
